acabefdd09ae5eb6f0ac487c43d13da75809b133
mjnkool <hoangdinhthangloi@gmail.com>
2020-06-17 01:31:25 +0700

    ....

--- admin/delete.php
+++ admin/delete.php
@@ -2,24 +2,24 @@
 include 'adminfunctions.php';
 $pdo = pdo_connect_mysql();
 $msg = '';
-// Check that the contact ID exists
+
 if (isset($_GET['id'])) {
-    // Select the record that is going to be deleted
+
     $stmt = $pdo->prepare('SELECT * FROM products WHERE id = ?');
     $stmt->execute([$_GET['id']]);
     $product = $stmt->fetch(PDO::FETCH_ASSOC);
     if (!$product) {
         exit('Contact doesn\'t exist with that ID!');
     }
-    // Make sure the user confirms beore deletion
+
     if (isset($_GET['confirm'])) {
         if ($_GET['confirm'] == 'yes') {
-            // User clicked the "Yes" button, delete record
+    
             $stmt = $pdo->prepare('DELETE FROM products WHERE id = ?');
             $stmt->execute([$_GET['id']]);
             $msg = 'Xóa sản phẩm thành công!';
         } else {
-            // User clicked the "No" button, redirect them back to the read page
+           
             header('Location: home.php');
             exit;
         }
--- admin/home.php
+++ admin/home.php
@@ -1,7 +1,7 @@
 <?php
-// We need to use sessions, so you should always start sessions using the below code.
+
 session_start();
-// If the user is not logged in redirect to the login page...
+
 if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit;
@@ -10,21 +10,21 @@ if (!isset($_SESSION['loggedin'])) {
 
 <?php
 include 'adminfunctions.php';
-// Connect to MySQL database
+
 $pdo = pdo_connect_mysql();
-// Get the page via GET request (URL param: page), if non exists default the page to 1
+
 $page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int) $_GET['page'] : 1;
-// Number of records to show on each page
+
 $records_per_page = 5;
 
-// Prepare the SQL statement and get records from our contacts table, LIMIT will determine the page
+
 $stmt = $pdo->prepare('SELECT * FROM products ORDER BY id LIMIT :current_page, :record_per_page');
 $stmt->bindValue(':current_page', ($page - 1) * $records_per_page, PDO::PARAM_INT);
 $stmt->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
 $stmt->execute();
-// Fetch the records so we can display them in our template.
+
 $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// Get the total number of contacts, this is so we can determine whether there should be a next and previous button
+
 $num_products = $pdo->query('SELECT COUNT(*) FROM products')->fetchColumn();
 ?>
 
